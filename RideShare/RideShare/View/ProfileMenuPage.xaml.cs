11d35b2ff2e5163aed6c9fc16a7d8530ac0422b0
mahesh <mahesh.wahalawatta@gmail.com>
2016-07-14 15:46:38 +0530

    Added new source code for the menu page

--- RideShare/RideShare/View/ProfileMenuPage.xaml.cs
+++ RideShare/RideShare/View/ProfileMenuPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RideShare.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace RideShare.View
         public ProfileMenuPage() 
         {
             InitializeComponent();
+            BindingContext = new ProfileMenuViewModel(Navigation);
         }
     }
 }
---
+++ RideShare/RideShare/ViewModel/ProfileMenuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RideShare.ViewModel
+{
+    public class ProfileMenuViewModel : INotifyPropertyChanged
+    {
+        private ProfileMenuViewModel() { }
+        public ProfileMenuViewModel(INavigation navigation)
+        {
+            EditProfileImageSource = ImageSource.FromResource("RideShare.Images.navigation.edit.png");
+            HistoryImageSource = ImageSource.FromResource("RideShare.Images.navigation.history.png");
+            SettingsImageSource = ImageSource.FromResource("RideShare.Images.navigation.settings.png");
+            HelpImageSource = ImageSource.FromResource("RideShare.Images.navigation.help.png");
+            AboutImageSource = ImageSource.FromResource("RideShare.Images.navigation.about.png");
+            LogoutImageSource = ImageSource.FromResource("RideShare.Images.navigation.logout.png");
+        }
+         
+        public ImageSource EditProfileImageSource { protected set; get; }
+        public ImageSource HistoryImageSource { protected set; get; } 
+        public ImageSource SettingsImageSource { protected set; get; }
+        public ImageSource HelpImageSource { protected set; get; }
+        public ImageSource AboutImageSource { protected set; get; }
+        public ImageSource LogoutImageSource { protected set; get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
